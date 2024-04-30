5561062fa598b29dd78fcf41600e7994e501fb69
kennethmasih <160132642+kennethmasih@users.noreply.github.com>
2024-04-30 21:27:47 +0000

    edited seismic

--- src/services/seismicService.ts
+++ src/services/seismicService.ts
@@ -1,27 +1,27 @@
 import { faker } from "@faker-js/faker";
 
-export const generateLondonseismicData = (): seismicData => {
-  // Generate random weather data
-  const generatedseismicData = {
+export const generateLondonSeismicData = (): SeismicData => {
+  // Generate random Seismic data
+  const generatedSeismicData = {
     temperature: faker.number.int({ min: -15, max: 30 }),
     humidity: faker.number.int({ min: 79, max: 86 }),
     wind: faker.number.int({ min: 2, max: 78 }),
     rain: faker.number.int({ min: 65, max: 75 }),
   };
 
-  // Return weather data
-  return generatedseismicData;
+  // Return Seismic data
+  return generatedSeismicData;
 };
 
-export const generateDublinseismicData = (): seismicData => {
-  // Generate random weather data
-  const generatedseismicData: seismicData = {
+export const generateDublinSeismicData = (): SeismicData => {
+  // Generate random Seismic data
+  const generatedSeismicData: SeismicData = {
     temperature: faker.number.int({ min: -15, max: 30 }),
     humidity: faker.number.int({ min: 79, max: 86 }),
     wind: faker.number.int({ min: 2, max: 78 }),
     rain: faker.number.int({ min: 65, max: 75 }),
   };
 
-  // Return seismic data
-  return generatedseismicData;
+  // Return Seismic data
+  return generatedSeismicData;
 };
