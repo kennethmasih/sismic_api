a2e12a9c3859e3eec72e04e3feacbe03d0fde89b
kennethmasih <160132642+kennethmasih@users.noreply.github.com>
2024-04-30 21:27:10 +0000

    edited seismic

--- src/routes/seismicRoute.ts
+++ src/routes/seismicRoute.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import { getseismicData } from "../controllers/seismicController.js";
+import { getSeismicData } from "../controllers/SeismicController.js";
 import { validateCityName } from "../middleware/validators.js";
 
 // We will create a router object
 const router = express.Router();
 
-// We will create a route for the seismic data based on the city name
-router.get("/:city", validateCityName, getseismicData);
+// We will create a route for the Seismic data based on the city name
+router.get("/:city", validateCityName, getSeismicData);
 
 // We will export the router
 export default router;
